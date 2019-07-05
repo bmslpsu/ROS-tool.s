17f5f4ab8fb40720f6d6bd473276c32b34c8d6ea
BC <boc5244@psu.edu>
2019-07-04 23:52:59 -0400

    Initial Commit

---
+++ run_retrack.py
@@ -0,0 +1,10 @@
+
+from retrack import Retrack
+
+RUN = Retrack()
+
+root 		= '/media/jean-michel/My Book/EXPERIMENTS/Experiment_SOS_v2/bag'
+filespec 	= 'fly_1_*.bag'
+duration 	= 30
+
+RUN.run(root, filespec, duration)
